f40f77e3de4c5b53ac966170fefd25403ba1a75a
chiang.cw <chiang@chiangcwdeMacBook-Air.local>
2017-12-17 21:54:01 +0800

    first commit

---
+++ src/main/java/org/sample/admin/springbootadminweb/InfoEndpointContributor.java
@@ -0,0 +1,19 @@
+package org.sample.admin.springbootadminweb;
+
+
+import java.util.Collections;
+
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InfoEndpointContributor implements InfoContributor {
+
+    @Override
+    public void contribute(Info.Builder builder) {
+        builder.withDetail("example",
+                Collections.singletonMap("key", "value"));
+    }
+
+}
